286498127736f0909f359616c13f327063d181d3
AlrxDaley <alexander_daley@icloud.com>
2022-02-17 12:15:00 +0000

    prepeard to deoply to heroku

--- django_test/django_test/settings.py
+++ django_test/django_test/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -74,11 +75,15 @@ WSGI_APPLICATION = 'django_test.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#       'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#}
+#}
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse('postgres://wzafwffvjbgvbb:081c6e09904a731ecea4dbc09477ddde44f62790e1646e21306f69f27671631f@ec2-34-230-110-100.compute-1.amazonaws.com:5432/d3ce8dsa20t89f')
 }
 
 
